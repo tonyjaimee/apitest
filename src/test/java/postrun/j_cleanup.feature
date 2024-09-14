368e61aa23e47ab1a032cd4a1b7335743f3e1a44
Pakpoom Thongsinthop <tony.t@Pakpooms-MBP.gateway>
2024-09-14 12:19:57 +1000

    14 Sept 2024

---
+++ src/test/java/postrun/j_cleanup.feature
@@ -0,0 +1,25 @@
+    # auto-format shortcut: Shift+Option+F
+Feature: j_cleanup_main
+Background:
+  * url baseUrl = baseUrl
+  
+  * def responseUser = karate.callSingle('j_cleanup_get_profile.feature')
+  * def currentUser = responseUser.currentUser
+
+  * def responseSessionList = karate.callSingle('j_cleanup_get_session_list.feature')
+  * def sessionList = responseSessionList.sessionList
+  
+  # Generate JSON payload used for DELETE /api/v1/session/remove_user_from_session
+  # E.g., the output will be {"session_id": "123", "user_id_to_remove": "user1"}
+  # How it works? "map" function will iterate through each element inside the sessionList, then create "user_id_to_remove" element then set value to the current user id
+  * def data = sessionList.map(x => { x.user_id_to_remove = currentUser.user_id; return x })
+  And print data
+
+  # Remove user from 1..N sessions
+  * def removeUserFromSession = call read('j_cleanup_remove_user_from_session.feature') data
+
+  # Remove 1..N sessions
+  * def deleteSession = call read('j_cleanup_delete_session.feature') sessionList
+
+Scenario: Clean up is completed
+   * print 'Clean up is completed'
--- src/test/java/tonyt/ParallelTestRunner.java
+++ src/test/java/tonyt/ParallelTestRunner.java
@@ -1,9 +1,14 @@
 package tonyt;
 
+// Java library
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
@@ -13,41 +18,146 @@ import com.intuit.karate.Runner;
 
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedDirectoryUpload;
+import software.amazon.awssdk.transfer.s3.model.DirectoryUpload;
+import software.amazon.awssdk.transfer.s3.model.UploadDirectoryRequest;
 
-// # auto-format shortcut: Shift+Option+F
+// # auto-format shortcut: Shift+Option+F, screenshot: shift+command+3, reveal hidden file in Mac OS:  Shift + Control (command) + dot
 
 public class ParallelTestRunner {
 
+    // Testing variables
+    private static String FEATURE_FILE_CLEANUP_LOCATION;
+    private static String FEATURE_FILE_TEST_LOCATION;
+
+    // S3 connection variables
+    private static String S3_BUCKET_NAME;
+    private static String S3_SOURCE_FOLDER;
+    private static String S3_ACCESS_KEY;
+    private static String S3_SECRET;
+
+    // Contructor
+    public ParallelTestRunner() {
+        super();
+    }
+
+    // Main method
     @Test
-    void testParallel() {
-        // you can run a specific feature file
-        // Runner.path("classpath:tonyt/j_profile.feature").tags("~@ignore") to speed up your test
-        
-        // Run clean up job first to prevent process deadlock-https://stackoverflow.com/questions/60943891/dynamic-scenario-freezes-when-called-using-afterfeature-hook/60944060#60944060
-        Results cleanup = Runner.path("classpath:postrun/j_cleanup.feature").tags("~@ignore")
+    public void runTest() {
+       
+        setParameters();
+        runParallelTest();
+        uploadToS3();
+    }
+
+    private void setParameters() {
+        try {
+            Properties prop = new Properties();
+
+            /*
+             * To create .properties file
+             * 1) Create "package" called src.test.resources (not folder)
+             * 2) Add the file "config.properties"
+             * 3) under POM.XML, you need to add below line to instruct Maven to copy your .properties file into the target directory.
+             * Why? Because your code will try to find the .properties file at target folder (where you deploy)
+             *  <testResource>
+                    <directory>src/test/resources</directory>
+                    <filtering>true</filtering>
+                </testResource>
+             */
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream("config.properties");
+
+            if (stream == null) {
+                System.out.println("Unable to find config.properties");
+                return;
+            }
+            prop.load(stream);
+
+            FEATURE_FILE_CLEANUP_LOCATION = prop.getProperty("featurefile.cleanup.location");
+            FEATURE_FILE_TEST_LOCATION = prop.getProperty("featurefile.test.location");
+            S3_BUCKET_NAME = prop.getProperty("s3.bucketname");
+            S3_SOURCE_FOLDER = prop.getProperty("s3.sourcefolder");
+            S3_ACCESS_KEY = prop.getProperty("s3.accesskey");
+            S3_SECRET = prop.getProperty("s3.secret");
+            
+            //System.out.println(prop.getProperty("featurefile.cleanup.location"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Methods
+    private void runParallelTest() {
+        /*
+         * You can run a specific feature file to speed up your test
+         * / e.g., Runner.path("classpath:tonyt/j_profile.feature").tags("~@ignore")
+         * 
+         * / Run clean up job first to prevent process deadlock
+         * / https://stackoverflow.com/questions/60943891/dynamic-scenario-freezes-when-
+         * called-using-afterfeature-hook/60944060#60944060
+         */
+
+        Results cleanup = Runner.path(FEATURE_FILE_CLEANUP_LOCATION).tags("~@ignore")
                 .outputCucumberJson(false)
                 .parallel(1);
-        
-        Results results = Runner.path("classpath:tonyt").tags("~@ignore")
+
+        Results results = Runner.path(FEATURE_FILE_TEST_LOCATION).tags("~@ignore")
                 .outputCucumberJson(true)
                 .parallel(2);
         generateReport(results.getReportDir());
-        System.out.print("report dir=>" + results.getReportDir());
-       
     }
 
-    public static void generateReport(String karateOutputPath) {
+    private void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
-        for (int i = 0; i < jsonPaths.size(); i++) {
-            System.out.println("json path=>" + jsonPaths.get(i));
-        }
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-
         Configuration config = new Configuration(new File("target"), "jaimee");
-
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
 
+    public S3TransferManager createS3TransferManager() {
+
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(
+                S3_ACCESS_KEY,
+                S3_SECRET);
+        Region region = Region.AP_SOUTHEAST_2;
+        S3AsyncClient s3AsyncClient = S3AsyncClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .region(region)
+                .httpClientBuilder(NettyNioAsyncHttpClient.builder()
+                        .maxConcurrency(10)
+                        .maxPendingConnectionAcquires(1000))
+                .build();
+        S3TransferManager transferManager = S3TransferManager.builder()
+                .s3Client(s3AsyncClient)
+                .build();
+        return transferManager;
+    }
+
+    private void uploadToS3() {
+        try {
+            S3TransferManager s3TransferManager = createS3TransferManager();
+            DirectoryUpload directoryUpload = s3TransferManager.uploadDirectory(UploadDirectoryRequest.builder()
+                    .source(Paths.get(S3_SOURCE_FOLDER))
+                    .bucket(S3_BUCKET_NAME)
+                    .build());
+
+            // Wait for the transfer to complete
+            CompletedDirectoryUpload completedDirectoryUpload = directoryUpload.completionFuture().join();
+
+            // Print out any failed uploads
+            completedDirectoryUpload.failedTransfers().forEach(System.out::println);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
